889f79cd5f5ad923ce9c86fc6fb5f237cb5266fe
José Carlos Quiroga España <168389544+CarlosQE@users.noreply.github.com>
2026-02-26 13:43:51 -0400

    Add citations to area calculation methods

--- GeoApp/models/calculadora.cs
+++ GeoApp/models/calculadora.cs
@@ -4,13 +4,13 @@ namespace GeoApp.Models
 {
     public static class Calculadora
     {
-        [cite_start]// Lógica para el Desarrollador A 
+        [cite_start]// Lógica para el Desarrollador A: Rectángulo [cite: 30-32]
         public static double AreaRectangulo(double baseR, double altura) => baseR * altura;
 
-        [cite_start]// Lógica para el Desarrollador B
+        [cite_start]// Lógica para el Desarrollador B: Círculo [cite: 33-34]
         public static double AreaCirculo(double radio) => Math.PI * Math.Pow(radio, 2);
 
-        [cite_start]// Lógica para el Desarrollador C 
+        [cite_start]// Lógica para el Desarrollador C: Triángulo [cite: 35-37]
         public static double AreaTriangulo(double baseT, double altura) => (baseT * altura) / 2;
     }
 }
